22a6f8611bad8d337f78dc29eb99aac6da90450c
boa2017 <beatrizovejero@gmail.com>
2018-06-09 20:18:01 +0200

    Changes required after first submission

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,12 +2,8 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.example.android.tellJokesActivity.TellJokesActivity;
 
